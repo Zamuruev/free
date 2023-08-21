b9b6dad416a0146d3d3b9f6f70c5e72e0e59eaa8
first repo <r.zamuruev@vk.com>
2023-08-21 12:35:56 +0300

    first commit

---
+++ src/test/java/miit/uvp/free/FreeApplicationTests.java
@@ -0,0 +1,13 @@
+package miit.uvp.free;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
