9a71181980488ee491f8dd83cd7f8bf1b50ef960
Alena <1138725@edu.rut-miit.tu>
2023-08-21 17:20:46 +0300

    repo+serviceIMPL

--- src/main/java/miit/uvp/free/repositories/SchoolClassRepository.java
+++ src/main/java/miit/uvp/free/repositories/SchoolClassRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 
 @Repository
 public interface SchoolClassRepository extends JpaRepository<SchoolClass,Long> {
+    List<Student>findSchoolClassByName(String nameStudent);
 }
--- src/main/java/miit/uvp/free/services/impl/SchoolClassServiceImpl.java
+++ src/main/java/miit/uvp/free/services/impl/SchoolClassServiceImpl.java
@@ -1,14 +1,36 @@
 package miit.uvp.free.services.impl;
 
+import miit.uvp.free.dtos.SchoolClassDTO;
 import miit.uvp.free.repositories.SchoolClassRepository;
 import miit.uvp.free.services.SchoolClassService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SchoolClassServiceImpl implements SchoolClassService<Long> {
     @Autowired
     private SchoolClassRepository schoolClassRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
 
+    @Override
+    public List<SchoolClassDTO> getAll(){
+        return schoolClassRepository.findAll().stream().map((s)->modelMapper.map(s,SchoolClassDTO.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<SchoolClassDTO> findSchoolClass(Long id){
+        return Optional.ofNullable(modelMapper.map(schoolClassRepository.findById(id),SchoolClassDTO.class));
+    }
+
+    @Override
+    public List<SchoolClassDTO> findSchoolClassByStudent(String nameStudent){
+        return schoolClassRepository.findSchoolClassByName(nameStudent).stream().map((s)->modelMapper.map(s,SchoolClassDTO.class)).collect(Collectors.toList());
+    }
 }
